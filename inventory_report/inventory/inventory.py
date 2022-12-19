ac59b3fade5f6361f91d465d7effb1be6614f083
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-19 20:49:30 -0300

    feat: Gerando relatórios através de um arquivo xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,7 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
-""" import xmltodict """
+import xmltodict
 
 
 class Inventory:
@@ -12,6 +12,8 @@ class Inventory:
             return Inventory().open_csv(path, type)
         elif 'json' in path:
             return Inventory().open_json(path, type)
+        elif 'xml' in path:
+            return Inventory().open_xml(path, type)
         raise ValueError()
 
     # Conteúdo do dia 1.2
@@ -28,8 +30,23 @@ class Inventory:
 
     @classmethod
     def open_json(self, path, type):
-        with open(path) as file:
+        with open(path, 'r') as file:
             inventory_file = json.load(file)
+            if type == "simples":
+                return SimpleReport().generate(list(inventory_file))
+            elif type == "completo":
+                return CompleteReport().generate(list(inventory_file))
+            else:
+                raise ValueError
+
+    # Install xmltodict: https://pypi.org/project/xmltodict/
+    # Using xmltodict: https://docs.python-guide.org/scenarios/xml/
+    # https://docs.python-guide.org/scenarios/xml/
+    # https://acervolima.com/ler-e-escrever-arquivos-xml-em-python/
+    @classmethod
+    def open_xml(self, path, type):
+        with open(path) as file:
+            inventory_file = xmltodict.parse(file.read())["dataset"]["record"]
             if type == "simples":
                 return SimpleReport().generate(list(inventory_file))
             elif type == "completo":
