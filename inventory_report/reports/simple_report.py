9aa92337b475263c5a79942e2299818bdcf45226
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-19 15:36:57 -0300

    feat: Gerando a versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, lista):
+        oldest_date = min(
+            [product["data_de_fabricacao"] for product in lista]
+        )
+
+        closest_date = min(
+            [product["data_de_validade"] for product in lista]
+        )
+
+        company_bigger_stock = Counter(
+            [product["nome_da_empresa"] for product in lista]
+        ).most_common(1)[0][0]
+
+        # Forma alternativa de encontrar a empresa com mais produtos:
+        """ company_bigger_stock = max(
+            Counter(product["nome_da_empresa"] for product in lista)
+        ) """
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {closest_date}\n"
+            f"Empresa com mais produtos: {company_bigger_stock}"
+        )
+
+# Referências
+# most_common()[][]:
+    # https://stackoverflow.com/questions/1518522/find-the-most-common-element-in-a-list
