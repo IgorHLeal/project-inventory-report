a57b814b0867246417b447b959f2568d22cacfac
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-19 20:04:31 -0300

    feat: Gerando relatórios através de um arquivo csv

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,24 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
+""" import json """
+""" import xmltodict """
+
+
+class Inventory:
+    @classmethod
+    def import_data(self, path, type):
+        if 'csv' in path:
+            return Inventory().open_csv(path, type)
+
+    # Conteúdo do dia 1.2
+    @classmethod
+    def open_csv(self, path, type):
+        with open(path, encoding="utf-8") as file:
+            inventory_file = csv.DictReader(file, delimiter=",", quotechar='"')
+            if type == "simples":
+                return SimpleReport().generate(list(inventory_file))
+            elif type == "completo":
+                return CompleteReport().generate(list(inventory_file))
+            else:
+                raise ValueError
