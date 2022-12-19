f6b872cdb578cd92e2d0988abcec96b51d587d2d
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-19 10:05:01 -0300

    test: Implementando teste para o inicializador do objeto do produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Takamine Jumbo GJ72 CE NT",
+        "Takamine",
+        "15/10/2010",
+        "20/12/2030",
+        "TKMNE24122019",
+        "Armazenar sempre no case"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Takamine Jumbo GJ72 CE NT"
+    assert product.nome_da_empresa == "Takamine"
+    assert product.data_de_fabricacao == "15/10/2010"
+    assert product.data_de_validade == "20/12/2030"
+    assert product.numero_de_serie == "TKMNE24122019"
+    assert product.instrucoes_de_armazenamento == "Armazenar sempre no case"
