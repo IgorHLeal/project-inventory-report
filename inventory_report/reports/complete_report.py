1eb82dd2c8aea67f240512d1b1fc2d3b7aef0567
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-19 19:17:14 -0300

    feat: Gerando a versão completa do relatório

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, product):
+        simple_report = super().generate(product)
+
+        products = Counter(
+            value["nome_da_empresa"] for value in product
+        )
+
+        result = ""
+
+        for company in products:
+            result += f"- {company}: {products[company]}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{result}"
+        )
