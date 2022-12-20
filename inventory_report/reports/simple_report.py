cd76e4ef37b0901895f889d20c3cca626cbcbc57
Igor Leal <igorhenriqueleal3@gmail.com>
2022-12-20 17:54:23 -0300

    refactor: Mudando a lógica com a utilização do datetime, como é recomoendado no README

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,16 +1,20 @@
 from collections import Counter
+from datetime import datetime
 
 
 class SimpleReport:
     @classmethod
     def generate(self, lista):
+        date = datetime.now().strftime('%Y-%m-%d')
+
         oldest_date = min(
             [product["data_de_fabricacao"] for product in lista]
         )
 
-        closest_date = min(
-            [product["data_de_validade"] for product in lista]
-        )
+        closest_date = min((
+            [product["data_de_validade"] for product in lista
+                if product["data_de_validade"] > date]
+        ))
 
         company_bigger_stock = Counter(
             [product["nome_da_empresa"] for product in lista]
@@ -30,3 +34,6 @@ class SimpleReport:
 # Referências
 # most_common()[][]:
     # https://stackoverflow.com/questions/1518522/find-the-most-common-element-in-a-list
+# datetime: https://docs.python.org/3/library/datetime.html
+# .now():
+    # https://www.freecodecamp.org/news/python-datetime-now-how-to-get-todays-date-and-time/
